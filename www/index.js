d2ed2281bef22bd2b00e91ceffa9365879a0b0b9
Seiji Shii <seiji.shii@gmail.com>
2017-07-18 18:48:15 +0900

    Refactored unused lines.

--- www/index.js
+++ www/index.js
@@ -6,21 +6,22 @@ let initialize = () => {
 }
 
 let onDeviceReady = () => {
-    console.log(device.platform);
-    if (device.platform !== 'iOS' && device.platform !== 'Android') {
+    console.log('Platform: ' + device.platform);
+    if (device.platform === 'iOS' || device.platform === 'Android') {
 
+        
+    } else {
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDmr4KjAGEvMjcmDdR7G6LdBIutoAAA2Yo&callback=initMap';
+        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDmr4KjAGEvMjcmDdR7G6LdBIutoAAA2Yo&callback=initBrowserMap';
         
-        console.dir(document.body);
+        // console.dir(document.body);
         document.body.appendChild(script);
-        // currentScript.parentNode.insertBefore(script, document.currentScript);
     }
 }
 
 var map;
-function initMap() {
+function initBrowserMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
